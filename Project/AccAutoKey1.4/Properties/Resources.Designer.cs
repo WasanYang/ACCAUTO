5a9133db5fcbd11e75b0c6e36204249a60906e9a
wy <wy@manaosoftware.com>
2024-08-27 20:54:41 +0700

    update source code to v5

--- Project/AccAutoKey1.4/Properties/Resources.Designer.cs
+++ Project/AccAutoKey1.4/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AccAutoKey4.Properties {
+namespace ACCautokey.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace AccAutoKey4.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AccAutoKey4.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ACCautokey.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
